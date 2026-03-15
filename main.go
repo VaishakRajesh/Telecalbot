8112fb71632dd830d41048ffdab6c8e61e8fdb35
Vaishak <vaishakrajesh42@gmail.com>
2026-03-15 14:48:26 +0530

    error update

--- main.go
+++ main.go
@@ -53,6 +53,9 @@ func main() {
 
 	r := gin.Default()
 
+	r.GET("/", func(c *gin.Context) {
+		c.JSON(200, gin.H{"status": "running"})
+	})
 	r.POST("/webhook", func(c *gin.Context) {
 
 		var update tgbotapi.Update
